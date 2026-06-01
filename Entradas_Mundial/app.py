5a780cd8321138796434f88af22c025ad9db436f
tomynaveda-design <tomynaveda@gmail.com>
2026-06-01 18:11:52 -0300

    Tercer commit

--- Entradas_Mundial/app.py
+++ Entradas_Mundial/app.py
@@ -6,6 +6,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['GET'])
+def mostrar_login():
+    return render_template('iniciosesion.html')
+
+@app.route('/login', methods=['POST'])
+def procesar_login():
+    username = request.form['username']
+    password = request.form['password']
+    
+    if username == 'admin' and password == 'password':
+        return render_template('bienvenido', username=username)
+    else:
+        return render_template('iniciosesion.html', error='Credenciales incorrectas')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
