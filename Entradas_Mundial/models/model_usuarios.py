f4ea856bd247145af0ad1e5f25257b3f7dbc34e4
tomynaveda-design <tomynaveda@gmail.com>
2026-06-14 21:43:17 -0300

    Creación tabla usuarios

---
+++ Entradas_Mundial/models/model_usuarios.py
@@ -0,0 +1,69 @@
+from Entradas_Mundial import db
+
+
+class UsuarioBase(db.Model):
+    __tablename__ = 'usuarios'
+    
+    id=db.Column(db.Integer, primary_key=True)
+    nombre=db.Column(db.String(50), nullable=False)
+    apellido=db.Column(db.String(50), nullable=False)
+    email=db.Column(db.String(50), nullable=True)
+    contraseña=db.Column(db.String(50), nullable=False)
+    rol=db.Column(db.String(50), nullable=False)
+    
+    
+    __mapper_args__ = {
+        'polymorphic_on': rol
+    }
+    
+    
+    def iniciar_sesion(self):
+        pass
+    
+    def cerrar_sesion(self):
+        pass
+    
+    def editar_contraseña(self):
+        pass
+    
+    
+class UsuarioCliente(db.model):
+    __tablename__ = 'usuarios_cliente'
+    
+    id=db.Column(db.Integer, db.ForeignKey('usuarios.id'), primary_key=True)
+    
+    
+    __mapper_args__ = {
+        'polymorphic_identity': 'cliente'
+    }
+    
+compras =db.relationship("Compra",backref="cliente", lazy=True)
+
+def bucar_partidos(self):
+    pass
+
+def seleccionar_partido(self):
+    pass
+
+def comprar_entradas(self):
+    pass
+
+class Administrador(db.Model):
+    __tablaname__ = 'administradores'
+    
+    id =db.Column(db.Interger, db.ForeignKey('usuarios.id'), primary_key=True)
+    
+    dni= db.Column(db.String(50), unique=True, nullable=False)
+    
+    __mapper_args__ = {
+        'polymorphic_identity': 'administrador'
+    }
+    
+def visualizar_general(self):
+    pass
+
+def editar_datos(self):
+    pass
+
+def editar_datos_permitidos(self):
+    pass
